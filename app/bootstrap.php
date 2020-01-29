19af38ec35e948b13e2b9a0ebbec5a2d0fbb91f9
Serhiy Bolkun <serhij16@live.de>
2020-01-29 07:46:49 +0100

    security, draw.io reworked

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -8,6 +8,7 @@ require_once 'helpers/json_helper.php';
 require_once 'helpers/mysql_helper.php';
 require_once 'helpers/session_helper.php';
 require_once 'helpers/url_helper.php';
+
 // Autoload Core Libraries
 spl_autoload_register(function($className){
     require_once 'libraries/' . $className . '.php';
