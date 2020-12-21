5220d42adcdf7dd1c3494b5230077cb36936b4d5
Serhiy Bolkun <serhij16@live.de>
2020-12-21 21:55:54 +0100

    Rollback changes with pagination, cause it happens if form resubmitted!

--- app/views/inc/3b-all-page-content.php
+++ app/views/inc/3b-all-page-content.php
@@ -145,30 +145,4 @@
         </nav>
     </div>
 </nav>
-<script>
-    // pagination navbar display always at a bottom of a page
-    window.onresize = function(event) {
-        var x = $(window).height();
-        if ($('body').height() > x) {
-            // scrollable
-            document.getElementById("pagination").style.position = "relative";
-            document.getElementById("pagination").style.bottom = "0";
-            document.getElementById("pagination").style.width = "100%";
-        } else {
-            document.getElementById("pagination").style.position = "absolute";
-            document.getElementById("pagination").style.bottom = "0";
-            document.getElementById("pagination").style.width = "100%";
-        }
-    };
-    var x = $(window).height();
-    if ($('body').height() > x) {
-        // scrollable
-        document.getElementById("pagination").style.position = "relative";
-        document.getElementById("pagination").style.bottom = "0";
-        document.getElementById("pagination").style.width = "100%";
-    } else {
-        document.getElementById("pagination").style.position = "absolute";
-        document.getElementById("pagination").style.bottom = "0";
-        document.getElementById("pagination").style.width = "100%";
-    }
-</script>
+<script src="<?php echo PUBLIC_CORE_JSURL . '/pagination/pagination.js'; ?>"></script>
