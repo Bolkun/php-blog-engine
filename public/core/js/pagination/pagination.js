3f1601b83b69b4c894381025c74cf737d468c75f
Serhiy Bolkun <serhij16@live.de>
2020-12-22 11:20:26 +0100

    Fixed display Pagination always at a bottom

--- public/core/js/pagination/pagination.js
+++ public/core/js/pagination/pagination.js
@@ -1,25 +1,21 @@
-// pagination navbar display always at a bottom of a page
-window.onresize = function(event) {
-    var x = $(window).height();
-    if ($('body').height() > x) {
+/*
+ * pagination navigation display always at a bottom of a page
+ */
+function setPaginationBottom(){
+    // Check if body height is higher than window height :)
+    if ($("html").height() > $(window).height()) {
         // scrollable
         document.getElementById("pagination").style.position = "relative";
-        document.getElementById("pagination").style.bottom = "0";
-        document.getElementById("pagination").style.width = "100%";
     } else {
         document.getElementById("pagination").style.position = "absolute";
-        document.getElementById("pagination").style.bottom = "0";
-        document.getElementById("pagination").style.width = "100%";
     }
-};
-var x = $(window).height();
-if ($('body').height() > x) {
-    // scrollable
-    document.getElementById("pagination").style.position = "relative";
-    document.getElementById("pagination").style.bottom = "0";
-    document.getElementById("pagination").style.width = "100%";
-} else {
-    document.getElementById("pagination").style.position = "absolute";
-    document.getElementById("pagination").style.bottom = "0";
-    document.getElementById("pagination").style.width = "100%";
 }
+
+window.onresize = function() {
+    setPaginationBottom();
+};
+
+// detect a fully-loaded page for STRG + F5
+window.addEventListener("load", function(){
+    setPaginationBottom();
+});
