7178baee5a4c32aeb9b20e5dce939f1773a07dea
Serhiy Bolkun <serhij16@live.de>
2020-12-30 21:13:22 +0100

    test index page view reworked

--- app/views/index/tests/index.php
+++ app/views/index/tests/index.php
@@ -2,15 +2,15 @@
 <?php require APPROOT . '/views/inc/2-nav-top-user.php'; ?>
 <?php require APPROOT . '/views/inc/3-nav-top-admin.php'; ?>
     <br><br><br>
-    <div class="container-fluid">
+    <div class="container">
         <div class="row">
-            <div class="col-sm-2"></div>
-            <div class="col-sm-8">
+            <div class="col-lg-12">
                 <div id="wrapper">
                     <h1><?php echo $data['title']; ?></h1>
                     <div class="row">
                         <div class="col-sm-4">
-                            <img width="100%" src="<?php echo PUBLIC_CORE_IMG_DEVURL; ?>/testing_pyramid1400x1031.png" class="rounded d-block" alt="logo">
+                            <img width="100%" src="<?php echo PUBLIC_CORE_IMG_DEVURL; ?>/testing_pyramid1400x1031.png"
+                                 class="rounded d-block" alt="logo">
                         </div>
                         <div class="col-sm-8">
                             <h2>Intro</h2>
@@ -43,23 +43,23 @@
                             <tr>
                                 <td>Integration Tests</td>
                                 <td>
-                                    <a href="<?php echo URLROOT; ?>/index/tests/benchmark">performance testing (benchmarks)</a>
+                                    <a href="<?php echo URLROOT; ?>/index/tests/benchmark">performance testing
+                                        (benchmarks)</a>
                                 </td>
                             </tr>
                             <?php foreach ($data['aHelpersFiles'] as $helper) { ?>
-                            <tr>
-                                <td>Unit Tests</td>
-                                <td>
-                                    <a href="<?php echo URLROOT; ?>/index/tests/helpers/<?php echo deleteCharsInStringBasedOnPosition($helper, -4); ?>">helpers/<?php echo $helper; ?></a>
-                                </td>
-                            </tr>
+                                <tr>
+                                    <td>Unit Tests</td>
+                                    <td>
+                                        <a href="<?php echo URLROOT; ?>/index/tests/helpers/<?php echo deleteCharsInStringBasedOnPosition($helper, -4); ?>">helpers/<?php echo $helper; ?></a>
+                                    </td>
+                                </tr>
                             <?php } ?>
                             </tbody>
                         </table>
                     </div>
                 </div>
             </div>
-            <div class="col-sm-2"></div>
         </div>
     </div>
 <?php require APPROOT . '/views/inc/4-footer.php'; ?>
